93e1e14ef9fafd6bc6eb4de1ea9e896cf4af8c2f
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-23 16:46:52 +0530

    Update 14_if-else.cpp

--- 14_if-else.cpp
+++ 14_if-else.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    // Input from user
+    cout << "Enter a number: ";
+    cin >> number;
+
+    // if-else condition
+    if (number > 0) {
+        cout << "The number is positive." << endl;
+    }
+    else if (number < 0) {
+        cout << "The number is negative." << endl;
+    }
+    else {
+        cout << "The number is zero." << endl;
+    }
+
+    return 0;
+}
 
