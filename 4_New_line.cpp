b818ac17791bd388a884a89f4e8bf62fdf3d7315
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-23 11:15:00 +0530

    Update 4_New_line.cpp

--- 4_New_line.cpp
+++ 4_New_line.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    // in this program , we will use \n to create3 new line
+    cout << "Jai \n";
+    cout << "Shree \n";
+    cout << "Krishna \n";
+
+    return 0;
+}
