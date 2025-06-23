10684ac39b28fe76ed7b5d153a5457dbb5046bc1
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-23 11:18:14 +0530

    Update and rename comments.cpp to 5_comments.cpp

---
+++ 5_comments.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // this is a single line comments
+    cout << "Jai \n";
+    cout << "Shree \n";
+    cout << "Krishna \n";
+    /*this is 
+    multiline 
+    comments*/
+
+    return 0;
+}
