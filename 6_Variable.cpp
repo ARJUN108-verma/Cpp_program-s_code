8ff1a0e74edb5535bdd95a0c43a6f62c609d45c2
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-23 11:58:38 +0530

    Update and rename Variable.cpp to 6_Variable.cpp

---
+++ 6_Variable.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // we will see variables
+    int num1 = 9;
+    int num2 = 27;
+    int sum =  num1 + num2;
+    float num3 = 27.27;
+    char let1 = 'S';
+    string text ="Arjun Maurya";
+    bool boolvalue = true;
+    
+    cout << "this is " << num1 <<" value of num1 \n";
+    cout << "this is " << num3 << " value of num3 \n";
+    cout << "this is " << let1 <<" value of let1 \n";
+    cout << "this is " << text <<" value of text \n";
+    cout << "this is " << boolvalue <<" value of boolvalue\n";
+    cout << "this is " << sum <<" value of sum ";
+
+    return 0;
