6ec1adecb7dc72cf5bb4c369cf0929c4074bb7b0
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-23 17:18:57 +0530

    Update 28_function.cpp

--- 28_function.cpp
+++ 28_function.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b) {
+    return a + b;
+}
+
+double add(double a, double b) {
+    return a + b;
+}
+
+int main() {
+    cout << "Int Addition: " << add(2, 3) << endl;
+    cout << "Double Addition: " << add(2.5, 3.7) << endl;
+    return 0;
+}
 
