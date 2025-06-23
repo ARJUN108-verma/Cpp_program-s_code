3e1f2a6dbfe23875813b2f6abdce40c208d53f8a
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-23 16:33:23 +0530

    Update 9_data_types.cpp

--- 9_data_types.cpp
+++ 9_data_types.cpp
@@ -1,1 +1,6 @@
-
+int myNum = 5;               
+float myFloatNum = 5.99;     
+double myDoubleNum = 9.98;   
+char myLetter = 'D';         
+bool myBoolean = true;       
+string myText = "Hello";     
