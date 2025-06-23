d014b85e3f38cf46390eb5052d053fb9672cb7f9
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-23 21:49:05 +0530

    Update 29_class_and_object.cpp

--- 29_class_and_object.cpp
+++ 29_class_and_object.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
+
+// Class definition
+class Student {
+public:
+    // Data members
+    string name;
+    int age;
+    float marks;
+
+    // Member function
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Marks: " << marks << endl;
+    }
+};
+
+int main() {
+    // Object creation
+    Student s1;
+
+    // Assigning values to object
+    s1.name = "Arjun";
+    s1.age = 20;
+    s1.marks = 87.5;
+
+    // Calling member function
+    s1.display();
+
+    return 0;
+}
 
