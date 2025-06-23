547587a477741f3561107179d03a218821d4f797
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-23 08:55:28 +0530

    Update 3_statement.cpp

--- 3_statement.cpp
+++ 3_statement.cpp
@@ -1,1 +1,11 @@
+//first program
+#include<iostream>
 
+using namespace std;
+
+int main() {
+    cout << "Hello World! \n";
+    cout << "Have a good day! \n";
+    cout << "jai ho \n" ;
+    return 0;
+}
